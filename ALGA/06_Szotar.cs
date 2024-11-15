7fdf6f5361728360042739d68737620808123d58
MateOE0991 <pappmate0991@gmail.com>
2024-11-15 22:23:55 +0100

    10

--- ALGA/06_Szotar.cs
+++ ALGA/06_Szotar.cs
@@ -6,101 +6,217 @@ using System.Threading.Tasks;
 
 namespace OE.ALGA.Adatszerkezetek
 {
-    public class SzotarElem<K,T>
-    {
-        public K Kulcs;
-        public T Tart;
+    //public class SzotarElem<K,T>
+    //{
+    //    public K Kulcs;
+    //    public T Tart;
 
+    //    public SzotarElem(K kulcs, T tart)
+    //    {
+    //        Kulcs = kulcs;
+    //        Tart = tart;
+    //    }
+    //}
+
+
+
+    //public class HasitoSzotarTulcsordulasiTerulettel<K, T> : Szotar<K, T>
+    //{
+    //    SzotarElem<K, T>[] E;
+    //    Func<K, int> h;
+    //    private LancoltLista<SzotarElem<K, T>> U;
+
+    //    public HasitoSzotarTulcsordulasiTerulettel(int meret, Func<K, int> hasitoFuggveny)
+    //    {
+    //        E = new SzotarElem<K, T>[meret];
+    //        h = x => hasitoFuggveny(x) % E.Length;
+    //        U = new LancoltLista<SzotarElem<K, T>>();
+    //    }
+
+    //    public HasitoSzotarTulcsordulasiTerulettel(int meret) : this(meret, kulcs => kulcs.GetHashCode())
+    //    {
+    //    }
+
+    //    private SzotarElem<K,T> KulcsKeres(K kulcs)
+    //    {
+    //        SzotarElem<K, T> e = null;
+
+    //        if (E[h(kulcs)] != null && EqualityComparer<K>.Default.Equals(E[h(kulcs)].Kulcs, kulcs))
+    //        {
+    //            return E[h(kulcs)];
+    //        }
+    //        if (U != null)  // Ellenőrizzük, hogy létezik-e a láncolt lista
+    //        {
+    //            U.Bejar(x =>
+    //            {
+    //                if (EqualityComparer<K>.Default.Equals(x.Kulcs, kulcs))
+    //                {
+    //                    e = x;  // Ha megtaláltuk, elmentjük a talált elemet
+
+    //                }
+    //            });
+    //        }
+    //        return e;
+    //    }
+
+    //    public void Beir(K kulcs, T ertek)
+    //    {
+    //        SzotarElem<K, T> x = KulcsKeres(kulcs); 
+
+    //        if( x != null)
+    //        {
+    //            E[h(kulcs)].Tart = ertek;
+    //        }
+
+    //        if  (E[h(kulcs)] == null)
+    //        {
+    //            E[h(kulcs)] = new SzotarElem<K, T>(kulcs, ertek);
+    //        }
+    //        else { U.Hozzafuz(new SzotarElem<K, T>(kulcs,ertek)); }
+    //    }
+
+    //    public T Kiolvas(K kulcs) 
+    //    {
+    //        SzotarElem<K,T> x = KulcsKeres(kulcs);
+    //        if (x != null)
+    //        {
+    //            return x.Tart;
+    //        }
+    //        else throw new HibasKulcsKivetel();
+
+    //    }
+
+    //    public void Torol(K kulcs)
+    //    {
+    //        if (E[h(kulcs)] != null && EqualityComparer<K>.Default.Equals(E[h(kulcs)].Kulcs, kulcs))
+    //        E[h(kulcs)] = null;
+    //        else
+    //        {
+    //            U.Bejar(x =>
+    //            {
+    //                if (EqualityComparer<K>.Default.Equals(x.Kulcs, kulcs))
+    //                {
+    //                   U.Torol(x);
+    //                }
+    //            });
+    //        }
+    //    }
+
+    //}
+
+    // SzotarElem<K, T> osztaly
+    public class SzotarElem<K, T>
+    {
+        public K kulcs; // Publikus mezo a kulcs tarolasahoz
+        public T tart;  // Publikus mezo a tartalom tarolasahoz
+
+        // Konstruktor a kulcs es tartalom beallitasahoz
         public SzotarElem(K kulcs, T tart)
         {
-            Kulcs = kulcs;
-            Tart = tart;
+            this.kulcs = kulcs;
+            this.tart = tart;
         }
     }
 
-   
-
+    // HasitoSzotarTulcsordulasiTerulettel<K, T> osztaly
     public class HasitoSzotarTulcsordulasiTerulettel<K, T> : Szotar<K, T>
     {
-        SzotarElem<K, T>[] E;
-        Func<K, int> h;
-        private LancoltLista<SzotarElem<K, T>> U;
+        private SzotarElem<K, T>[] E; // Adattarolo tomb
+        private Func<K, int> h; // Hasito fuggveny
+        private LancoltLista<SzotarElem<K, T>> U; // Tulcsordulasi terulet
 
+        // Konstruktor meret es hasito fuggveny megadasaval
         public HasitoSzotarTulcsordulasiTerulettel(int meret, Func<K, int> hasitoFuggveny)
         {
             E = new SzotarElem<K, T>[meret];
-            h = x => hasitoFuggveny(x) % E.Length;
             U = new LancoltLista<SzotarElem<K, T>>();
+
+            // Kompozit hasito fuggveny maradekos osztassal
+            h = kulcs => hasitoFuggveny(kulcs) % E.Length;
         }
 
-        public HasitoSzotarTulcsordulasiTerulettel(int meret) : this(meret, static kulcs => kulcs.GetHashCode())
+        // Konstruktor csak meret megadasaval (alapertelmezett hasitofuggveny hasznalata)
+        public HasitoSzotarTulcsordulasiTerulettel(int meret)
+            : this(meret, kulcs => kulcs.GetHashCode())
         {
         }
 
-        private SzotarElem<K,T> KulcsKeres(K kulcs)
+        // Privat kulcskereses metodus
+        private SzotarElem<K, T> KulcsKeres(K kulcs)
         {
-            SzotarElem<K, T> e = null;
-
-            if (E[h(kulcs)] != null && EqualityComparer<K>.Default.Equals(E[h(kulcs)].Kulcs, kulcs))
+            int index = h(kulcs);
+            if (E[index] != null && EqualityComparer<K>.Default.Equals(E[index].kulcs, kulcs))
             {
-                return E[h(kulcs)];
+                return E[index];
             }
-            if (U != null)  // Ellenőrizzük, hogy létezik-e a láncolt lista
+
+            // Kereses a lancolt listaban
+            SzotarElem<K, T> x = null; 
+            
+            U.Bejar(elem =>
             {
-                U.Bejar(x =>
+                if (EqualityComparer<K>.Default.Equals(elem.kulcs, kulcs))
                 {
-                    if (EqualityComparer<K>.Default.Equals(x.Kulcs, kulcs))
-                    {
-                        e = x;  // Ha megtaláltuk, elmentjük a talált elemet
-                        
-                    }
-                });
-            }
-            return e;
+                    x = elem; 
+                }
+
+            });
+            return x; // Ha nincs meg a kulcs
         }
 
+        // Beir metodus
         public void Beir(K kulcs, T ertek)
         {
-            SzotarElem<K, T> x = KulcsKeres(kulcs); 
-
-            if( x != null)
+            var meglevoElem = KulcsKeres(kulcs);
+            if (meglevoElem != null)
             {
-                E[h(kulcs)].Tart = ertek;
+                meglevoElem.tart = ertek; // Ha van mar ilyen kulcs, frissitjuk az erteket
+                return;
             }
-            
-            if  (E[h(kulcs)] == null)
+
+            var ujElem = new SzotarElem<K, T>(kulcs, ertek);
+            int index = h(kulcs);
+            if (E[index] == null)
             {
-                E[h(kulcs)] = new SzotarElem<K, T>(kulcs, ertek);
+                E[index] = ujElem; // Ures helyre irunk
             }
-            else { U.Hozzafuz(new SzotarElem<K, T>(kulcs,ertek)); }
-        }
-
-        public T Kiolvas(K kulcs) 
-        {
-            SzotarElem<K,T> x = KulcsKeres(kulcs);
-            if (x != null)
-            {
-                return x.Tart;
-            }
-            else throw new HibasKulcsKivetel();
-
-        }
-
-        public void Torol(K kulcs)
-        {
-            if (E[h(kulcs)] != null && EqualityComparer<K>.Default.Equals(E[h(kulcs)].Kulcs, kulcs))
-            E[h(kulcs)] = null;
             else
             {
-                U.Bejar(x =>
-                {
-                    if (EqualityComparer<K>.Default.Equals(x.Kulcs, kulcs))
-                    {
-                       U.Torol(x);
-                    }
-                });
+                U.Hozzafuz(ujElem); // Tulcsordulasi teruletre fuzunk
             }
         }
 
+        // Kiolvas metodus
+        public T Kiolvas(K kulcs)
+        {
+            var elem = KulcsKeres(kulcs);
+            if (elem != null)
+            {
+                return elem.tart; // Ha van ilyen kulcs, visszaadjuk az erteket
+            }
+
+            throw new HibasKulcsKivetel(); // Ha nincs meg, kivetelt dobunk
+        }
+
+        // Torol metodus
+        public void Torol(K kulcs)
+        {
+            int index = h(kulcs);
+            if (E[index] != null && EqualityComparer<K>.Default.Equals(E[index].kulcs, kulcs))
+            {
+                E[index] = null; // Torles a tombbol
+                return;
+            }
+
+            var elem = KulcsKeres(kulcs);
+            if (elem != null)
+            {
+                U.Torol(elem); // Torles a lancolt listabol
+                return;
+            }
+
+            throw new HibasKulcsKivetel(); // Ha nincs meg, kivetelt dobunk
+        }
     }
+
 }
---
+++ ALGA/10_SulyozatlanGraf.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.ALGA.Adatszerkezetek
+{ 
+
+    // EgeszGrafEl osztály, amely a gráf éleit reprezentálja
+    public class EgeszGrafEl : GrafEl<int>, IComparable
+    {
+        public int Honnan { get; }
+        public int Hova { get; }
+
+        public EgeszGrafEl(int honnan, int hova)
+        {
+            Honnan = honnan;
+            Hova = hova;
+        }
+
+        public int CompareTo(EgeszGrafEl other)
+        {
+            if (Honnan != other.Honnan)
+                return Honnan.CompareTo(other.Honnan);
+            return Hova.CompareTo(other.Hova);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is EgeszGrafEl other)
+                return Honnan == other.Honnan && Hova == other.Hova;
+            return false;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj != null && obj is EgeszGrafEl b)
+            {
+                if (b.Honnan != Honnan)
+                {
+                    return Honnan.CompareTo(b.Honnan);
+                }
+                else
+                {
+                    return Hova.CompareTo(b.Hova);
+                }
+            }
+            else throw new InvalidOperationException();
+        }
+
+    }
+
+    // CsucsmatrixSulyozatlanEgeszGraf osztály
+    public class CsucsmatrixSulyozatlanEgeszGraf : SulyozatlanGraf<int, EgeszGrafEl>
+    {
+        private readonly int n;
+        private readonly bool[,] M;
+
+        public CsucsmatrixSulyozatlanEgeszGraf(int n)
+        {
+            this.n = n;
+            M = new bool[n, n];
+        }
+
+        public int CsucsokSzama => n;
+
+        public int ElekSzama
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (M[i, j])
+                            count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        public Halmaz<int> Csucsok
+        {
+            get
+            {
+                Halmaz<int> halmaz = new FaHalmaz<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    halmaz.Beszur(i);
+                }
+                return halmaz;
+            }
+        }
+
+        public Halmaz<EgeszGrafEl> Elek
+        {
+            get
+            {
+                Halmaz<EgeszGrafEl> halmaz = new FaHalmaz<EgeszGrafEl>();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (M[i, j])
+                        {
+                            halmaz.Beszur(new EgeszGrafEl(i, j));
+                        }
+                    }
+                }
+                return halmaz;
+            }
+        }
+
+        public void UjEl(int honnan, int hova)
+        {
+            M[honnan, hova] = true;
+        }
+
+        public bool VezetEl(int honnan, int hova)
+        {
+            return M[honnan, hova];
+        }
+
+        public Halmaz<int> Szomszedai(int csucs)
+        {
+            FaHalmaz<int> halmaz = new FaHalmaz<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (M[csucs, i])
+                {
+                    halmaz.Beszur(i);
+                }
+            }
+            return halmaz;
+        }
+    }
+
+  //   GráfBejárások osztály
+    public static class GrafBejarasok
+    {
+        public static Halmaz<V> SzelessegiBejaras<V, E>(Graf<V, E> g, V start, Action<V> muvelet) where V : IComparable
+        {
+          Sor<V> S = new LancoltSor<V>();
+          Halmaz<V> F = new FaHalmaz<V>();
+            S.Sorba(start);
+            F.Beszur(start);
+          
+          while (!S.Ures)
+          {
+                V k = S.Sorbol();
+                muvelet(k);
+               
+                Halmaz<V> szomszedok = g.Szomszedai(k);
+
+                szomszedok.Bejar(szomszed =>
+                {
+                    if (!F.Eleme(szomszed))
+                    {
+                        S.Sorba(szomszed);
+                        F.Beszur(szomszed);
+                    }
+
+                });
+          }
+          return F;
+
+        }
+
+        public static Halmaz<V> MelysegiBejaras<V, E>(Graf<V, E> g, V start, Action<V> muvelet) where V : IComparable
+        {
+            var latogatott = new FaHalmaz<V>();
+            MelysegiBejarasRekurzio(g, start, latogatott, muvelet);
+            return latogatott;
+        }
+
+        private static void MelysegiBejarasRekurzio<V, E>(Graf<V, E> g, V k, Halmaz<V> F, Action<V> muvelet) where V : IComparable
+        {
+            if (F.Eleme(k))
+                return;
+
+            F.Beszur(k);
+            muvelet(k);
+
+            Halmaz<V> szomszedok = g.Szomszedai(k);
+
+            szomszedok.Bejar(szomszed =>
+            {
+                MelysegiBejarasRekurzio(g, szomszed, F, muvelet);
+            });
+
+        }
+    }
+
+}
